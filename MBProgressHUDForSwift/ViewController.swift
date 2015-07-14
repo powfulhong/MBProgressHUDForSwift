30791a57057b1f6feab568de1adcb0f66e91c562
powfulhong <970087302@qq.com>
2015-07-14 19:30:58 +0800

    init project

---
+++ MBProgressHUDForSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MBProgressHUDForSwift
+//
+//  Created by hzs on 15/7/14.
+//  Copyright (c) 2015年 powfulhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
